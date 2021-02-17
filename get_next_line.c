8d5e8e20a2232c0f64379a1ae4263239bf807046
EarlyObject <sr.ardak@gmail.com>
2021-02-17 19:03:13 +0100

    interim fixes

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: asydykna <asydykna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/11 08:40:14 by asydykna          #+#    #+#             */
-/*   Updated: 2021/02/17 15:45:56 by asydykna         ###   ########.fr       */
+/*   Updated: 2021/02/17 19:01:43 by asydykna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,33 +14,22 @@
 
 int get_next_line(int fd, char **line)
 {
-	printf("FUNCTION CALLED\n");
-	static unsigned int BUFFER_SIZE;
-	BUFFER_SIZE = 32;
-	size_t i;
 	static size_t btsread;
-	char *buf;
+	static size_t flag;
 	static char *tail;
+	size_t i;
+	char *buf;
 	char *ptr;
 	char *temp;
-	static size_t offset;
-	
-	if (fd < 0 || !line)
-		return (-1);
-	if (!(buf = (char*)malloc(BUFFER_SIZE)))
-		return (-1);
 
+	printf("BUFFER SIZE IS %d\n", BUFFER_SIZE);
+	if (fd < 0 || !line || !(buf = (char *)malloc(BUFFER_SIZE)))
+		return (-1);
 	ptr = NULL;
-	temp = NULL;
-	//free(ptr);
-	//free(temp);
-
-	while (offset || (btsread = read(fd, buf, BUFFER_SIZE)) > 0)
+	while (flag || (btsread = read(fd, buf, BUFFER_SIZE)) > 0)
 	{
-		if (offset)
-		{
+		if (flag)
 			buf = tail;
-		}
 		if (!(temp = (char *)malloc(btsread)))
 			return (-1);
 		i = 0;
@@ -49,29 +38,28 @@ int get_next_line(int fd, char **line)
 			if (buf[i] == '\n' || buf[i] == EOF)
 				break;
 			*(temp + i) = *(buf + i);
-			i++;	
+			i++;
 		}
 		temp[i] = '\0';
-		//stick ptr and temp
 		ptr = ptr ? ft_strjoin(ptr, temp) : temp;
 		*line = ptr;
 		if (i < btsread)
 		{
-			//создаем с размером учитывая что не нужна ячейка для символа переноса строки
 			if (!(tail = (char *)malloc(btsread - i)))
 				return (-1);
-			//добавляем 1, так как надо проскочить перенос строки
 			ft_strlcpy(tail, buf + i + 1, btsread - i);
-			offset = i;
-			btsread = btsread - i;
-			i = 0;
+			free(buf);
+			flag = 1;
+			btsread = (i == 0) ? btsread - 1 : btsread - i;
 			return (1);
 		} else
 		{
-			offset = 0;
-			i = 0;
+			flag = 0;
 			btsread = 0;
 		}
 	}
+	//free(buf);*
+	//free(tail);
+	printf("HERE\n");
 	return (btsread == 0 ? 0 : -1);
 }
--- makefile
+++ makefile
@@ -6,7 +6,7 @@
 #    By: asydykna <asydykna@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/01/26 13:13:58 by asydykna          #+#    #+#              #
-#    Updated: 2021/02/15 09:25:34 by asydykna         ###   ########.fr        #
+#    Updated: 2021/02/17 18:54:26 by asydykna         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -21,7 +21,9 @@ RM = rm -f
 
 CFLAGS = -Wall -Wextra -Werror
 
+BUF = -D BUFFER_SIZE=32
+
 all: program
 
 program:
-	$(CC) $(CFLAGS) $(SRC)
+	$(CC) $(CFLAGS) $(BUF) $(SRC)
